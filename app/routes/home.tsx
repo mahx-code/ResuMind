bb3e8c4ca27f4661bd835ab84a4a798f677874ce
Mahmud <mahxgithub@gmail.com>
2025-12-30 15:52:55 +0100

    feat: add home page route with meta tags and basic UI

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,25 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResuMind" },
+    {
+      name: "description",
+      content:
+        "An AI-powered analyzer that instantly decodes resumes to provide actionable insights and optimization scores for job seekers and recruiters.",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your Applications & Resume Ratings</h1>
+          <h2>Upload your resume and get instant feedback on your application status and resume ratings.</h2>
+        </div>
+      </section>
+    </main>
+  );
 }
